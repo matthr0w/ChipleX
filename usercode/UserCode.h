0488a2e96353750aac2a5ff71ba8a2600ccb6c6b
Matthias Schlemmer <matthias.schlemmer@tum.de>
2025-08-20 19:36:37 +0200

    Add tests

--- usercode/UserCode.h
+++ usercode/UserCode.h
@@ -24,77 +24,32 @@ inline std::map<CoreKey, CoreFunctions> core_code = {
     // Chiplet1 Core0
     {{1, 0},
      {[](Core &core, UtilizationTracker *tracker) {
-        std::cout << "### Simultaneous Requests Example" << std::endl;
-        const size_t num_bytes = 8;
+        const size_t num_bytes = 16;
         uint8_t *data = new uint8_t[num_bytes];
 
         for (size_t i = 0; i < num_bytes; ++i) {
           data[i] = static_cast<uint8_t>(i);
         }
 
-        std::cout << "Core0 Buffer contents (" << num_bytes
-                  << " bytes):" << std::endl;
-        for (size_t i = 0; i < num_bytes; ++i) {
-          std::cout << "data[" << i << "] = " << static_cast<unsigned>(data[i])
-                    << std::endl;
-        }
+        auto h = core.send_request(TLM_WRITE_COMMAND, 1, 1, 0x1000, true, true,
+                                   reinterpret_cast<unsigned char *>(data),
+                                   num_bytes, 2, 8, 1);
 
-        auto h1 = core.send_request(TLM_WRITE_COMMAND, 1, 1, 0x1000, true, true,
-                                    reinterpret_cast<unsigned char *>(data),
-                                    num_bytes, 2, 4, 1);
-        SC_LOG_DEBUG_NO_TX(&core, "Core0 can continue...");
+        SC_LOG_DEBUG_NO_TX(&core, "WRITE OP - CACHE SKIP - CORE CONTINUE");
 
-        h1->wait();
+        h->wait();
 
-        wait(400, SC_NS);
+        SC_LOG_DEBUG_NO_TX(&core, "WRITE OP - CACHE SKIP - DATA READY");
 
-        std::cout << "### AXI Fixed Burst Example" << std::endl;
+        h = core.send_request(TLM_READ_COMMAND, 2, 1, 0x1000, true, false,
+                              reinterpret_cast<unsigned char *>(data),
+                              num_bytes, 2, 8, 1);
 
-        auto h2 = core.send_request(TLM_READ_COMMAND, 2, 1, 0x1000, true, true,
-                                    reinterpret_cast<unsigned char *>(data),
-                                    num_bytes, 2, 4, 0);
-        SC_LOG_DEBUG_NO_TX(&core, "Core0 can continue...");
+        SC_LOG_DEBUG_NO_TX(&core, "READ OP - CACHE USE - CORE CONTINUE");
 
-        h2->wait();
+        h->wait();
 
-        std::cout << "Core0 Buffer contents (" << num_bytes
-                  << " bytes):" << std::endl;
-        for (size_t i = 0; i < num_bytes; ++i) {
-          std::cout << "data[" << i << "] = " << static_cast<unsigned>(data[i])
-                    << std::endl;
-        }
-
-        std::cout << "### AXI Cache Read Miss Example" << std::endl;
-
-        auto h3 = core.send_request(TLM_READ_COMMAND, 3, 1, 0x1000, true, false,
-                                    reinterpret_cast<unsigned char *>(data),
-                                    num_bytes, 2, 4, 0);
-        SC_LOG_DEBUG_NO_TX(&core, "Core0 can continue...");
-
-        h3->wait();
-
-        std::cout << "### AXI Cache Read Hit Example" << std::endl;
-
-        auto h4 = core.send_request(TLM_READ_COMMAND, 4, 1, 0x1000, true, false,
-                                    reinterpret_cast<unsigned char *>(data),
-                                    num_bytes, 2, 4, 0);
-        SC_LOG_DEBUG_NO_TX(&core, "Core0 can continue...");
-
-        h4->wait();
-      },
-      [](Core &core, UtilizationTracker *tracker,
-         tlm_generic_payload *transaction) {}}},
-    {{1, 1},
-     {[](Core &core, UtilizationTracker *tracker) {
-        const size_t num_bytes = 8;
-        uint8_t *data = new uint8_t[num_bytes];
-
-        auto h1 = core.send_request(TLM_READ_COMMAND, 11, 1, 0x1000, true, true,
-                                    reinterpret_cast<unsigned char *>(data),
-                                    num_bytes, 2, 4, 1);
-        SC_LOG_DEBUG_NO_TX(&core, "Core1 can continue...");
-
-        h1->wait();
+        SC_LOG_DEBUG_NO_TX(&core, "READ OP - CACHE USE - DATA READY");
 
         std::cout << "Core1 Buffer contents (" << num_bytes
                   << " bytes):" << std::endl;
@@ -103,8 +58,15 @@ inline std::map<CoreKey, CoreFunctions> core_code = {
                     << std::endl;
         }
 
-        delete h1;
+        h = core.send_request(TLM_WRITE_COMMAND, 3, 1, 0x1000, true, false,
+                              reinterpret_cast<unsigned char *>(data),
+                              num_bytes, 2, 8, 1);
+
+        SC_LOG_DEBUG_NO_TX(&core, "WRITE OP - CACHE USE - CORE CONTINUE");
+
+        h->wait();
+
+        SC_LOG_DEBUG_NO_TX(&core, "WRITE OP - CACHE USE - DATA READY");
       },
       [](Core &core, UtilizationTracker *tracker,
-         tlm_generic_payload *transaction) {}}},
-};
+         tlm_generic_payload *transaction) {}}}};
