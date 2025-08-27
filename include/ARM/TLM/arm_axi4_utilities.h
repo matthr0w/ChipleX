87d7805d56f4b3d06c83f1c9c54aa67b438697b3
Matthias Schlemmer <matthias.schlemmer@tum.de>
2025-08-27 15:35:19 +0200

    Add AXI user signals struct

--- include/ARM/TLM/arm_axi4_utilities.h
+++ include/ARM/TLM/arm_axi4_utilities.h
@@ -1,6 +1,31 @@
 #include "arm_axi4_payload.h"
 #include "arm_axi4_phase.h"
 
+struct UserSignals {
+  uint8_t core = 0;
+  uint8_t source = 0;
+  uint8_t destination = 0;
+  bool fixed_address = true;
+
+  uint64_t encode() const {
+    uint64_t val = 0;
+    val |= (static_cast<uint64_t>(core) & 0xFF) << 56;
+    val |= (static_cast<uint64_t>(source) & 0xFF) << 48;
+    val |= (static_cast<uint64_t>(destination) & 0xFF) << 40;
+    val |= (static_cast<uint64_t>(fixed_address) & 0x1) << 39;
+    return val;
+  }
+
+  static UserSignals decode(uint64_t val) {
+    UserSignals user;
+    user.core = static_cast<uint8_t>((val >> 56) & 0xFF);
+    user.source = static_cast<uint8_t>((val >> 48) & 0xFF);
+    user.destination = static_cast<uint8_t>((val >> 40) & 0xFF);
+    user.fixed_address = ((val >> 39) & 0x1) != 0;
+    return user;
+  }
+};
+
 inline ARM::AXI::Size get_axi_size(unsigned bits) {
   if (bits < 8 || (bits % 8) != 0)
     throw std::invalid_argument("AXI size must be a multiple of 8 and >= 8");
