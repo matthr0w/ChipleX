da5489192615805c682f8ff66a114b705a27b981
Matthias Schlemmer <matthias.schlemmer@tum.de>
2025-09-18 18:38:52 +0200

    Update test

--- include/usercode/UserCode.h
+++ include/usercode/UserCode.h
@@ -31,14 +31,34 @@ inline std::map<CoreKey, CoreFunctions> core_code = {
           data[i] = static_cast<uint8_t>(i);
         }
 
-        auto req = Core::WriteRequest(
-                       1, reinterpret_cast<unsigned char *>(data), num_bytes)
-                       .set_dest(2)
-                       .set_addr(0x1000)
-                       .skip_cache();
+        auto reqw = Core::WriteRequest(
+                        1, reinterpret_cast<unsigned char *>(data), num_bytes)
+                        .set_dest(2)
+                        .set_addr(0x1000)
+                        .skip_cache();
 
-        auto h = core.write(req);
+        auto h = core.write(reqw);
+
+        auto reqr = Core::ReadRequest(1, 0x1000,
+                                      reinterpret_cast<unsigned char *>(data),
+                                      num_bytes)
+                        .set_dest(2)
+                        .skip_cache();
+
+        h = core.read(reqr);
         h->wait();
+
+        std::cout << "Data Buffer" << std::endl;
+        for (size_t i = 0; i < num_bytes; ++i) {
+          std::cout << std::hex << std::setw(2) << std::setfill('0')
+                    << static_cast<int>(data[i]);
+          if ((i + 1) % 16 == 0)
+            std::cout << "\n";
+          else
+            std::cout << " ";
+        }
+
+        delete[] data;
 
         sc_stop();
       },
